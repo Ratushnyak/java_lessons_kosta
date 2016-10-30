b1127c6235093799eb4ac413c1df8becf5a2f705
Ratushnyak <kosta-mars19@ukr.net>
2016-10-30 14:02:43 +0300

    add class Point

--- sandbox/src/main/java/MyTest.java
+++ sandbox/src/main/java/MyTest.java
@@ -1,6 +1,7 @@
 public class MyTest{
 
      public static void main(String[] args){
-	System.out.println("Hello World!!!");
-}
+	     Point point = new Point(5.0, 2.0, 12.0, 7.0);
+         System.out.println(point.distance());
+    }
 }
